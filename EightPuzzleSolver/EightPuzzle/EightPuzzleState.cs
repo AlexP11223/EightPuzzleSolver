2e2a160bca7369b927bce86b95af26b0b05f06bb
Alex P <alex.pantec@gmail.com>
2016-04-20 16:14:18 +0300

    Optimization: check parent board when creating next states to avoid going back to the same board

--- EightPuzzleSolver/EightPuzzle/EightPuzzleState.cs
+++ EightPuzzleSolver/EightPuzzle/EightPuzzleState.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using EightPuzzleSolver.Search;
 
 namespace EightPuzzleSolver.EightPuzzle
@@ -6,23 +7,28 @@ namespace EightPuzzleSolver.EightPuzzle
     public class EightPuzzleState : IProblemState<EightPuzzleState>
     {
         public EightPuzzleState(Board board)
-            : this(board, 0, null)
+            : this(board, 0)
         {
         }
 
-        public EightPuzzleState(Board board, MoveDirection direction)
-            : this(board, 1, direction)
+        public EightPuzzleState(Board board, EightPuzzleState parent, MoveDirection direction)
+            : this(board, 1, parent, direction)
         {
         }
 
-        private EightPuzzleState(Board board, int cost, MoveDirection? direction = null)
+        private EightPuzzleState(Board board, int cost, EightPuzzleState parent = null, MoveDirection? direction = null)
         {
             Board = board;
             Cost = cost;
+            Parent = parent;
             Direction = direction;
         }
 
         public Board Board { get; }
+
+        public EightPuzzleState Parent { get; }
+
+        public bool IsRootState => Parent == null;
 
         /// <summary>
         /// Direction of the blank tile movement that was applied to the previous state
@@ -47,7 +53,11 @@ namespace EightPuzzleSolver.EightPuzzle
             {
                 if (Board.CanMove(direction))
                 {
-                    states.Add(new EightPuzzleState(Board.Move(direction), 1, direction));
+                    var board = Board.Move(direction);
+                    if (IsRootState || board != Parent.Board)
+                    {
+                        states.Add(new EightPuzzleState(board, this, direction));
+                    }
                 }
             }
 
